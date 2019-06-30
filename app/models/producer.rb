937b0a36f96f7c0fbf3f0236fbcf10333915b432
Sylvain Daubert <sylvain.daubert@laposte.net>
2019-06-30 12:28:54 +0200

    Producers and Providers: beautify show page.

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -3,4 +3,6 @@ class Producer < ApplicationRecord
   has_many :wines
 
   validates :name, presence: true
+  validates :web, url: { scheme: %w[http https] }, allow_blank: true
+  validates :email, format: /@/, allow_blank: true
 end
